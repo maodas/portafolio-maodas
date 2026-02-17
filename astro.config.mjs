13a3efec20ea78f2703605b2183d7e40e0776f77
maodas <rodas807@gmail.com>
2026-02-17 08:34:25 -0600

    Update astro.config.mjs and tsconfig.json

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
+/Documentos/portafolio-maodas/astro.config.mjs
 import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
-
 export default defineConfig({
-  site: 'https://maodas.online',
-  integrations: [tailwind()]
+  vite: {
+    ssr: {
+      external: []
+    }
+  }
 });
