2af6b85bac619b78af444dacbdb255dd8bc47233
maodas <rodas807@gmail.com>
2026-02-19 15:31:35 -0600

    Ajustes en la sección de blog creación de blogs de prueba que no solo incluyen texto sino tambien imagenes y videos.

--- src/content/config.ts
+++ src/content/config.ts
@@ -6,8 +6,17 @@ const blogCollection = defineCollection({
     title: z.string(),
     description: z.string(),
     publishDate: z.date(),
-    category: z.string(),
+    updatedDate: z.date().optional(),
+    author: z.string().default('Mao Das'),
+    image: z.string().optional(),
+    category: z.enum(['Tecnología', 'Desarrollo', 'Análisis de Datos', 'Automatización', 'Personal']),
+    tags: z.array(z.string()).optional(),
+    featured: z.boolean().default(false),
+    draft: z.boolean().default(false),
+    readTime: z.number().optional(),
   }),
 });
 
-export const collections = { blog: blogCollection };
+export const collections = {
+  blog: blogCollection,
+};
