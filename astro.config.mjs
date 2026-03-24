20aacc13c9e303c58cf12c69109aecc69281feb9
maodas <rodas807@gmail.com>
2026-03-23 22:45:14 -0600

    actualización de skills

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import mdx from '@astrojs/mdx';
+import icon from 'astro-icon';
 
 export default defineConfig({
   site: 'https://maodas.online',
 
-  integrations: [tailwind(), mdx()],
+  integrations: [tailwind(), mdx(), icon()],
   vite: {
     ssr: {
       external: []
